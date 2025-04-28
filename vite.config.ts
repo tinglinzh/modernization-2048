12786a29f1ef9a7ffb950e0e504aae60d50d478b
tinglinzh <liucheng0416@icloud.com>
2025-04-28 11:14:38 +0800

    feat: pwa支持

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),VitePWA({ 
+    registerType: 'autoUpdate',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3}'],
+    },
+   })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
