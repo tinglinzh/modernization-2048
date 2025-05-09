949d5f808c183c2d5215a7160fa41ec056aa8954
tinglinzh <liucheng0416@icloud.com>
2025-05-09 17:00:33 +0800

    chore: pwa配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,15 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),VitePWA({ 
+  plugins: [react(), tailwindcss(), VitePWA({
     registerType: 'autoUpdate',
     workbox: {
       globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3}'],
     },
-   })],
+    manifest: {
+      theme_color: "#ffffff",
+    },
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
